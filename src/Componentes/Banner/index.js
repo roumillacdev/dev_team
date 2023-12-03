f50a5a160db3c0d3fe74b1090a14ca7ee7cf5f1f
DeegohBR <digomillac@live.com>
2023-12-03 07:52:26 -0300

    First Commit

---
+++ src/Componentes/Banner/index.js
@@ -0,0 +1,13 @@
+import './Banner.css'
+
+const Banner = () => {
+    //  JSX
+    return(
+        <header className="banner">
+            <img src="/Imagens/banner.png" alt="Banner principal da página do Organo" />
+        </header>
+    )
+
+}
+
+export default Banner
---
+++ src/Componentes/Time/index.js
@@ -0,0 +1,28 @@
+import Colaborador from '../Colaborador'
+import './Time.css'
+
+const Time = (props) => {
+
+    const css = {backgroundColor: props.corSecundaria}
+    
+    const cssBorder = {borderColor: props.corPrimaria}
+
+    return(
+        (props.colaboradores.length > 0) && 
+        <section className="time" style={css}>
+            <h3 style={cssBorder}>{props.nome}</h3>
+            <div className="colaboradores">
+                {props.colaboradores.map
+                (colaborador => 
+                <Colaborador 
+                key={colaborador.nome}
+                corDeFundo={props.corPrimaria}
+                nome={colaborador.nome} 
+                cargo={colaborador.cargo} 
+                imagem={colaborador.imagem}  />)}
+            </div>
+        </section>
+    )
+}
+
+export default Time
